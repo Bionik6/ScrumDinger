71ce974e7376e5ef8a358816142d2344dd63327a
Ibrahima Ciss <bionik6@gmail.com>
2021-09-18 18:48:15 +0000

    Presenting the EditView in a full screen modal

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct DetailView: View {
-  var scrum: DailyScrum
+  let scrum: DailyScrum
+  @State private var isPresented = false
   
     var body: some View {
       List {
@@ -44,7 +45,14 @@ struct DetailView: View {
         }
       }
       .listStyle(InsetGroupedListStyle())
+      .navigationBarItems(
+        leading: Button("Cancel") { isPresented = false },
+        trailing: Button("Done") { isPresented = false }
+      )
       .navigationTitle(scrum.title)
+      .fullScreenCover(isPresented: $isPresented) {
+        NavigationView { EditView().navigationTitle(scrum.title) }
+        }
     }
 }
 
